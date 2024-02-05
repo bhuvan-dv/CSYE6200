8b7c101346b1a5cbefa5d7a6ba816a10c10f6ac0
D V Bhuvan <67942070+bhuvan-dv@users.noreply.github.com>
2024-02-04 21:20:32 -0500

    week 4

---
+++ week4/lab03-section02-group04-lab03/src/main/java/edu/neu/csye6200/Driver.java
@@ -0,0 +1,24 @@
+package edu.neu.csye6200;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ruchika Sharma
+ *
+ */
+
+public class Driver {
+	public static void main(String[] args) {
+		System.out.println("============Main Execution Start===================\n\n");
+		School.printStudents();
+
+
+
+
+
+		System.out.println("\n\n============Main Execution End===================");
+	}
+	//Add your code in between these two print statements
+
+}
